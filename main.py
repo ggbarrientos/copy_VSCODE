ef56f29fa5706da524e9f64ef1c443718569a35a
Barrientos <ggbarrientos@cps.k12.il.us>
2023-10-04 08:56:48 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#data abstraction- when you need to simplify data
+#from something more complex
+
+#An example of data extraction is When i use a list to simplfiy data
+#instead of having to write out the same code over and over again
+#data abtraction  is when you simplify data 
+#if you have a list of names, you cam simplify it by using list
